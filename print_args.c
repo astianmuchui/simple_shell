aa48ee9145b8038e61933b2d64e119630d18d076
Brendamasingu <brendamasingu@gmail.com>
2023-05-15 13:31:53 +0300

    fixed betty checks

--- print_args.c
+++ print_args.c
@@ -6,12 +6,12 @@
  * Return: pointer to total number
  */
 
-int *(_count) (char **array)
+int *_count(char **array)
 {
     int count = 0;
-    
-    int *count_ptr = malloc(sizeof(int)); 
-    
+
+    int *count_ptr = malloc(sizeof(int));
+
     if (count_ptr == NULL)
     {
         return (0);
@@ -24,32 +24,5 @@ int *(_count) (char **array)
     }
 
     *count_ptr = count;
-    return count_ptr;
-}
-
-/**
- * main - entry point
- * @argc: number of command line arguments
- * @argv: array of command line arguments
- * Return: always 0
- */
-
-int main(int ac, char **av)
-{
-    int *arr_count = _count(av);
-
-    if (*arr_count == ac) /* Confirms that the above function pointer works */
-    {
-        int j = 0;
-
-        while (j < *arr_count)
-        {
-            printf("%s\n",av[j]);
-            j++;
-        }
-    }
-
-
-    free(arr_count); 
-    return 0;
+    return (count_ptr;)
 }
