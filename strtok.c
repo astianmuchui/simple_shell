80f0493f10f65c00f3e7b47eae9813557f8721d9
Brendamasingu <brendamasingu@gmail.com>
2023-05-15 02:00:58 -0700

    updated source code

--- strtok.c
+++ strtok.c
@@ -9,31 +9,31 @@
 
 char *_strtok(char *str, const char *delim)
 {
-    static char *save;
-    char *token;
-    int i, j;
+	static char *save;
+	char *token;
+	int i, j;
 
-    if (str == NULL) /* Check if no string is provided */
-        str = save;
+	if(str == NULL) /* Check if no string is provided */
+		str = save;
 
-    for (i = 0; str[i] != '\0'; i++) /* loop through the string */
-    {
-        for (j = 0; delim[j] != '\0'; j++) /* loop through delimiter */
-        {
-            if (str[i] == delim[j]) /* Checks if delimiter is present in the string */
-            {
-                str[i] = '\0'; /* Null terminator: End of current function call */
-                save = &str[i + 1]; /* Pointer to next char: Next call goes from here */
-                return (str); 
-            }
-        }
-    }
+	for(i = 0; str[i] != '\0'; i++) /* loop through the string */
+	{	
+		for(j = 0; delim[j] != '\0'; j++) /* loop through delimiter */
+		{
+			if(str[i] == delim[j]) /* Checks if delimiter is present in the string */
+			{
+				str[i] = '\0'; /* Null terminator: End of current function call */
+				save = &str[i + 1]; /* Pointer to next char: Next call goes from here */
+				return (str); 
+			}
+		}
+	}
 
-    if (str == save)  /* No more tokens left */
-        return (NULL);
+	if(str == save)  /* No more tokens left */
+		return (NULL);
 
-    token = str;
-    save = NULL; /* Reset to Null */
+	token = str;
+	save = NULL; /* Reset to Null */
 
-    return (token);
+	return (token);
 }
