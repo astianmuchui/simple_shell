0a3c8b73e190c95b868ebd1093766c163509d21b
Brendamasingu <brendamasingu@gmail.com>
2023-05-15 12:04:26 +0300

    fixed betty checks

--- strtok.c
+++ strtok.c
@@ -13,23 +13,23 @@ char *_strtok(char *str, const char *delim)
 	char *token;
 	int i, j;
 
-	if(str == NULL) /* Check if no string is provided */
+	if (str == NULL) /* Check if no string is provided */
 		str = save;
 
-	for(i = 0; str[i] != '\0'; i++) /* loop through the string */
-	{	
-		for(j = 0; delim[j] != '\0'; j++) /* loop through delimiter */
+	for (i = 0; str[i] != '\0'; i++) /* loop through the string */
+	{
+		for (j = 0; delim[j] != '\0'; j++) /* loop through delimiter */
 		{
-			if(str[i] == delim[j]) /* Checks if delimiter is present in the string */
+			if (str[i] == delim[j]) /* Checks if delimiter is present in the string */
 			{
 				str[i] = '\0'; /* Null terminator: End of current function call */
 				save = &str[i + 1]; /* Pointer to next char: Next call goes from here */
-				return (str); 
+				return (str);
 			}
 		}
 	}
 
-	if(str == save)  /* No more tokens left */
+	if (str == save)  /* No more tokens left */
 		return (NULL);
 
 	token = str;
