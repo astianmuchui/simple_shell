c86da61169b32bdb1fb2cd60ac8774ede3219c8a
astianmuchui <sebastianmuchui79@gmail.com>
2023-05-22 17:19:37 +0300

    Modified header file

--- main.h
+++ main.h
@@ -6,6 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <dirent.h>
+#include <signal.h>
+
 
 pid_t _process_id(void);
 pid_t _process_ppid(void);
