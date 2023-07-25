e38b808a2b8df6fe2ec8176fee2ed228e3993293
brendamasingu <brendamasingu@gmail.com>
2023-07-24 22:30:35 -0700

    betty checks

--- memory.c
+++ memory.c
@@ -9,33 +9,34 @@
 
 void *_realloc(void *buffer, size_t size)
 {
-        size_t old_size = 64, cp_size;
-        void *new_buffer;
+	size_t old_size = 64, cp_size;
+	void *new_buffer;
 
-        /* If the buffer is null, allocate memory of size new_size */
-        if (buffer == NULL)
-        {
-                return malloc(size);
-        }
+	/* If the buffer is null, allocate memory of size new_size */
+	if (buffer == NULL)
+	{
+		return malloc(size);
+	}
 
-        if (size == 0)
-        { 
-                free(buffer);
-                return NULL;
-        }
-        /* New buffer size */
-        new_buffer = malloc(size);
+	if (size == 0)
+	{	
+		free(buffer);
+		return NULL;
+	}
+        
+	/* New buffer size */
+	new_buffer = malloc(size);
 
-        if (new_buffer == NULL)
-        {
-                return NULL;
-        }
+	if (new_buffer == NULL)
+	{
+		return NULL;
+	}
 
-        /* Copy the data from the old buffer to the new buffer */
-        cp_size = (old_size < size) ? old_size : size;
-        memcpy(new_buffer, buffer, cp_size);
+	/* Copy the data from the old buffer to the new buffer */
+	cp_size = (old_size < size) ? old_size : size;
+	memcpy(new_buffer, buffer, cp_size);
 
-        free(buffer);
+	free(buffer);
 
-        return new_buffer;
+	return new_buffer;
 }
