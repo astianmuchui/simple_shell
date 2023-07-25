ba09bad549786a452b3e564f6784ab8bbfbdac32
brendamasingu <brendamasingu@gmail.com>
2023-07-24 23:13:20 -0700

    betty check

--- arguments.c
+++ arguments.c
@@ -8,25 +8,25 @@
 
 char *_get_cmd_(void)
 {
-        char *line = NULL;
-        size_t bufsize = 0;
-        ssize_t characters;
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
 
-        characters = getline(&line, &bufsize, stdin);
-        if (characters == -1)
-        {
-                if (feof(stdin))
-                {
-                        printf("\n");
-                        exit(EXIT_SUCCESS);
-                }
-                else
-                {
-                        perror("readline error");
-                        exit(EXIT_FAILURE);
-                }
-        }
-        return (line);
+	characters = getline(&line, &bufsize, stdin);
+	if (characters == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("readline error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
 }
 
 /**
@@ -37,43 +37,44 @@ char *_get_cmd_(void)
 
 char **getArgs(char *line)
 {
-        int buffer_size = 64, i = 0;
+	int buffer_size = 64, i = 0;
 
-        char *token, **args = malloc(buffer_size * sizeof(char *));
+	char *token, **args = malloc(buffer_size * sizeof(char *));
 
-        if (!args)
-        {
-                fprintf(stderr, "Unable to allocate memory\n");
-                exit(EXIT_FAILURE);
-        }
+	if (!args)
+	{
+		fprintf(stderr, "Unable to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
 
-        token = _strtok(line, DELIM); /* Replace this with custom strtok later */
+	token = _strtok(line, DELIM); /* Replace this with custom strtok later */
 
-        while (token != NULL)
-        {
-                if (token[0] == '#')
-                {
-                        break;
-                }
+	while (token != NULL)
+	{
+		if (token[0] == '#')
+		{
+			break;
+		}
 
-                args[i] = token;
-                i++;
+		args[i] = token;
+		i++;
 
-                if (i >= buffer_size)
-                {
-                        buffer_size += 64;
-                        args = _realloc(args, buffer_size * sizeof(char *)); /* Replace this with custom realloc later */
-                        
-                        if (!args)
-                        {
-                                fprintf(stderr, "Unable to allocate memory\n");
-                                exit(EXIT_FAILURE);
-                        }
-                }
+		if (i >= buffer_size)
+		{
+			buffer_size += 64;
+			args = _realloc(args, buffer_size * sizeof(char *));
+			/* Replace this with custom realloc later */
 
-                token = _strtok(NULL, DELIM);
-        }
+			if (!args)
+			{
+				fprintf(stderr, "Unable to allocate memory\n");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        args[i] = NULL;
-        return (args);
+		token = _strtok(NULL, DELIM);
+	}
+
+	args[i] = NULL;
+	return (args);
 }
