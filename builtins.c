799df0f1625959a4243937f964d23f9254fdaaef
brendamasingu <brendamasingu@gmail.com>
2023-07-25 13:29:40 -0700

    betty checks

--- builtins.c
+++ builtins.c
@@ -2,77 +2,76 @@
 
 void printenv(void)
 {
-        char **env = environ;
-        int i = 0;
+	char **env = environ;
+	int i = 0;
 
-        for (i = 0; env[i] != NULL; i++)
-        {
-                printf("%s\n", env[i]);
-        }
+	for (i = 0; env[i] != NULL; i++)
+	{
+		printf("%s\n", env[i]);
+	}
 }
 
 int contains_builtin(char **arg)
 {
-        int i;
-        char *builtins[] = {
-                "cd",
-                "env",
-                "exit"
-        };
+	int i;
+	char *builtins[] = {
+		"cd",
+		"env",
+		"exit"
+	};
 
-        for (i = 0; builtins[i] != NULL; i++)
-        {
-                if (_str_contains(arg, (char **)builtins[i])) /* Typecast */
-                {
-                        return (1);
-                }
-                else
-                {
-                        return (0);
-                }
-        }
+	for (i = 0; builtins[i] != NULL; i++)
+	{
+		if (_str_contains(arg, (char **)builtins[i])) /* Typecast */
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
 
-        return (-1);
+	return (-1);
 }
 
 void cd(char **args)
 {
-        DIR *dir;
+	DIR *dir;
+	
+	if (args == NULL)
+	{
+		perror("Please provide a directory\n");
+	}
 
-        if (args == NULL)
-        {
-                perror("Please provide a directory\n");
-        }
+	/* Check if the directory actually exists */
+	dir = opendir(*args);
+	
+	if (dir == NULL)
+	{
+  		perror("Directory does not exist\n");
+	}
+	else
+	{
+		chdir(*args);
+		closedir(dir);
+	}
 
-        /* Check if the directory actually exists */
-
-        dir = opendir(*args);
-
-        if (dir == NULL)
-        {
-                perror("Directory does not exist\n");
-        }
-        else
-        {
-                chdir(*args);
-                closedir(dir);
-        }
 }
 
 void exit_shell(int arg)
 {
-        if (arg == 0)
-        {
-                exit(EXIT_SUCCESS);
-        }
-
-        else if (arg == 1)
-        {
-                exit(EXIT_FAILURE);
-        }
-
-        else
-        {
-                exit(arg);
-        }
+	if (arg == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	else if (arg == 1)
+	{
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		exit(arg);
+	}
 }
+
--- execute.c
+++ execute.c
@@ -7,62 +7,64 @@
  */
 int _exec_(char **args)
 {
-        pid_t pid;
-        int status;
-        char **envp;
+	pid_t pid;
+	int status;
+	char **envp;
 
-        if (args[0] == NULL)
-                return (-1);
-        pid = fork();
+	if (args[0] == NULL)
+		return (-1);
+	pid = fork();
 
-        if (pid == 0)
-        {
-                if (_cmd_isvalid(args[0])) /* Validate Command */
-                {        
-                       envp = returnenv();
+	if (pid == 0)
+	{
+		if (_cmd_isvalid(args[0])) /* Validate Command */
+	}	
+			envp = returnenv();
 
-                        if (execve(joinPath(BIN,*args), args, envp) == -1)
-                                perror("Unable to execute");
-                }
-        }
-        else if (pid < 0)
-        {
-                perror("errors in forking.\n");
-        }
-        else
-        {
-                while (!WIFEXITED(status) && !WIFSIGNALED(status))
-                {
-                        waitpid(pid, &status, WUNTRACED);
-                }
-        }
-        return (-1);
+			if (execve(joinPath(BIN, *args), args, envp) == -1)
+				perror("Unable to execute");
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("errors in forking.\n");
+	}
+
+	else
+	{
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+		{
+			waitpid(pid, &status, WUNTRACED);
+		}
+	}
+	return (-1);
 }
 
 /**
  * returnenv - returns environment variables
  * Return: array
  */
+
 char **returnenv(void)
 {
-        int envC = 0;
-        char **env = environ;
-        char **envp = malloc(sizeof(char *) * BUFSIZE);
+	int envC = 0;
+	char **env = environ;
+	char **envp = malloc(sizeof(char *) * BUFSIZE);
 
-        if (envp == NULL)
-        {
-                perror("Memory allocation error\n");
-                exit(EXIT_FAILURE);
-        }
+	if (envp == NULL)
+	{
+		perror("Memory allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-        while (*env != NULL)
-        {
-                envp[envC] = *env;
-                envC++;
-                env++;
-        }
+	while (*env != NULL)
+	{
+		envp[envC] = *env;
+		envC++;
+		env++;
+	}
 
-        envp[envC] = NULL;
+	envp[envC] = NULL;
 
-        return (envp);
+	return (envp);
 }
